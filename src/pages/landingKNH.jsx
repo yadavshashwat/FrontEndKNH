dde82c29e348ae6dc796aba580d31b63a8ee1312
yadavshashwat <y.shashwat@gmail.com>
2023-03-10 18:25:50 +0530

    Latest at KNH remove

--- src/pages/landingKNH.jsx
+++ src/pages/landingKNH.jsx
@@ -20,7 +20,6 @@ const BlogContent = [
         title: 'Sensitivity Child Development Clinic',
         category: 'Events - 11-Apr',
         link: childDevelopment
-
     }
     // {
     //     images: '02',
@@ -110,16 +109,8 @@ class landingKNH extends Component{
                 {/* End Slider Area   */}
 
                 {/* Start Blog Area */}
-                <div className="rn-blog-area pt--30 bg_color--1 mb-dec--30">
+                {/* <div className="rn-blog-area pt--30 bg_color--1 mb-dec--30">
                     <div className="container">
-                        {/* <div className="row align-items-end">
-                            <div className="col-lg-12 col-md-12 col-sm-12 col-12">
-                                <div className="section-title text-left">
-                                    <h2>Latest @ KNH</h2>
-                                    <p>Latest events and services for the all round care of our patients</p>
-                                </div>
-                            </div>
-                        </div> */}
                         <div className="row align-items-end">
                             <div className="col-lg-12">
                                 <div className="section-title service-style--3 text-center">
@@ -150,7 +141,7 @@ class landingKNH extends Component{
                             ))}
                         </div>    
                     </div>    
-                </div>
+                </div> */}
                 {/* End Blog Area */}
 
 
